f878f1731041bb6bf56b975d47bbc0c782148326
kolimaro <saithikus@gmail.com>
2020-05-19 13:37:48 +0800

    2.4.1

--- src/main/java/crudhibermvc/service/UserService.java
+++ src/main/java/crudhibermvc/service/UserService.java
@@ -1,21 +1,81 @@
 package crudhibermvc.service;
 
-import crudhibermvc.model.User;
 
+import crudhibermvc.entity.Role;
+import crudhibermvc.entity.User;
+import crudhibermvc.repository.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 import java.util.List;
 
 /**
- * @author Pavel Tokarev, 25.04.2020
+ * @author Pavel Tokarev, 19.05.2020
  */
 
-public interface UserService {
-    List<User> getAllUsers();
+@Service
+public class UserService implements UserDetailsService {
 
-    User getUserById(Long id);
+    @Autowired
+    private UserDao userDao;
 
-    void addUser(User user);
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-    void editUser(User user);
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByUsername(username);
 
-    void deleteUser(User user);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+
+    public User findUserById(Long userId) {
+        return userDao.findById(userId);
+    }
+
+    public User findUserByUsername(String username) {
+        return userDao.findByUsername(username);
+    }
+
+    public List<User> allUsers() {
+        return userDao.findAll();
+    }
+
+    public boolean saveUser(User user) {
+        User userFromDb = userDao.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        if (user.getRoles() == null) {
+            user.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userDao.save(user);
+        return true;
+    }
+
+    public void updateUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userDao.updateUser(user);
+    }
+
+    public boolean deleteUser(Long userId) {
+        User user = userDao.findById(userId);
+        if (user != null) {
+            userDao.deleteUser(user);
+            return true;
+        }
+        return false;
+    }
+
 }
